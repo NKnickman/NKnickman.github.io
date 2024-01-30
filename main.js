292f1c008c980a8e5d69398c0bd1c88ec937c306
Pikay421 <158016269+NKnickman@users.noreply.github.com>
2024-01-29 23:55:32 -0500

    Added Anatomical Humanoid and Multipurpose Humanoid Entries

--- main.js
+++ main.js
@@ -9,3 +9,53 @@ function include(path) {
 }
 
 include("stylesheet.css");
+
+for (const slideshow of document.getElementsByClassName("e_slideshow")) {
+    const content = document.createElement("div");
+    content.setAttribute("class", "e_slideshow_content");
+    while (slideshow.firstChild) {
+        const slide_content = slideshow.firstChild;
+        if (slide_content.tagName == null) { slideshow.removeChild(slide_content); continue; }
+
+        slide_content.style.display = "none";
+        slide_content.setAttribute("class", "e_slide");
+
+        // slideshow.removeChild(slide_content);
+        content.appendChild(slide_content);
+    }
+
+    const left_button = document.createElement("a");
+    slideshow.appendChild(left_button);
+    left_button.setAttribute("class", "e_slideshow_button");
+    left_button.innerHTML = "&#10094";
+    left_button.addEventListener("click", e => {
+        let children = content.children;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i].style.display == "block") {
+                console.log(children[i].tagName);
+                children[i].style.display = "none";
+                children[(i + children.length - 1) % children.length].style.display = "block";
+                break;
+            }
+        }
+    });
+
+    slideshow.appendChild(content);
+    content.firstChild.style.display = "block";
+
+    const right_button = document.createElement("a");
+    slideshow.appendChild(right_button);
+    right_button.setAttribute("class", "e_slideshow_button e_right");
+    right_button.innerHTML = "&#10095";
+    right_button.addEventListener("click", e => {
+        let children = content.children;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i].style.display == "block") {
+                console.log(children[i].tagName);
+                children[i].style.display = "none";
+                children[(i + 1) % children.length].style.display = "block";
+                break;
+            }
+        }
+    });
+}
