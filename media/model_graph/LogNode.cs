19d8500d252ca488b869aa7965bbc1f20ee3eaa5
Pikay421 <158016269+NKnickman@users.noreply.github.com>
2024-01-30 19:46:00 -0500

    Added Modelling Nodes Entry

---
+++ media/model_graph/LogNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace XNodeCore
+{
+    [CreateNodeMenu("Debug/Log")]
+    public class LogNode : CoreNode
+    {
+        public InSingleField<object> input = new InSingleField<object>();
+
+        public override void Process()
+        {
+            Debug.Log(input.Value);
+        }
+    }
+}
